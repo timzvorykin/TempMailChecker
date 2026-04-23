03808e7932ad44909a139b0660c45af46046dd68
timzvorykin <draghernofficial@gmail.com>
2026-04-23 13:52:50 +0300

    Добавление проекта

---
+++ Прокт для VS/TempMailChecker/TempMailChecker.h
@@ -0,0 +1,32 @@
+﻿
+// TempMailChecker.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CTempMailCheckerApp:
+// Сведения о реализации этого класса: TempMailChecker.cpp
+//
+
+class CTempMailCheckerApp : public CWinApp
+{
+public:
+	CTempMailCheckerApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTempMailCheckerApp theApp;
